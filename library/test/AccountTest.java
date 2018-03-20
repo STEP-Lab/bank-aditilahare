4b9e99361eda3a7f6248a9c4fce6b7eb4bc1d516
aditilahare <aditil@thoughtworks.com>
2018-03-20 14:48:04 +0530

    Added functionality to check accountNumber.

--- library/test/AccountTest.java
+++ library/test/AccountTest.java
@@ -12,4 +12,9 @@ public class AccountTest {
         Account account = new Account("1234", 1000);
         assertThat(account.getBalance(),is(1000));
     }
+    @Test
+    public void checkAccountNumber(){
+        Account account = new Account("1234", 100);
+        assertThat(account.getAccountNumber(),is("1234"));
+    }
 }
